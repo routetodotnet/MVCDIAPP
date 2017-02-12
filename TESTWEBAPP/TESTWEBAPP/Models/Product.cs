62a739b532c94eed0ba5f4badb3266903e3d5b4c
Amar <routetodotnet@gmail.com>
2017-02-12 13:02:10 -0500

    added product class file

---
+++ TESTWEBAPP/TESTWEBAPP/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TESTWEBAPP.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+
+    }
+}
